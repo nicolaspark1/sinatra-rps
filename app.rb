3d263535318aeeffc1c71858a8c0faf95e12c86c
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-10-22 02:40:07 +0000

    main choices

--- app.rb
+++ app.rb
@@ -13,3 +13,51 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  player = "rock"
+  computer_choices = ["rock", "paper", "scissors"]
+  computer = computer_choices.sample
+
+  if computer == "rock"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We tied!"  
+  end
+  if computer == "paper"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We lost!"  
+  end
+  if computer == "scissors"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We won!"  
+  end
+end
+
+get("/paper") do
+  player = "paper"
+  computer_choices = ["rock", "paper", "scissors"]
+  computer = computer_choices.sample
+
+  if computer == "paper"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We tied!"  
+  end
+  if computer == "scissors"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We lost!"  
+  end
+  if computer == "rock"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We won!"  
+  end
+end
+
+get("/scissors") do
+  player = "scissors"
+  computer_choices = ["rock", "paper", "scissors"]
+  computer = computer_choices.sample
+
+  if computer == "scissors"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We tied!"  
+  end
+  if computer == "rock"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We lost!"  
+  end
+  if computer == "paper"
+    @outcome = "We played #{player}! \n They played #{computer}! \n We won!"  
+  end
+end
